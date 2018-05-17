2a7b3975d3d20728373d040c35408dd8f4874335
Eugene Guzik <guzik.evgeny@gmail.com>
2018-05-17 21:09:00 +0300

    location updates added

--- app/src/main/java/com/android/example/pathfinder/TrackService.java
+++ app/src/main/java/com/android/example/pathfinder/TrackService.java
@@ -1,19 +1,35 @@
 package com.android.example.pathfinder;
 
+import android.Manifest;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
 public class TrackService extends Service {
     private static final String TAG = TrackService.class.getSimpleName();
     private static final int ONGOING_NOTIFICATION_ID = 111;
 
     private boolean isStarted = false;
+
+    private static final int LOCATION_INTERVAL = 1000;
+    private static final float LOCATION_DISTANCE = 1f;
+
+    private LocationCallback mLocationCallback;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Nullable
     @Override
@@ -25,7 +41,6 @@ public class TrackService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "onCreate");
-
 
         Intent notificationIntent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent =
@@ -41,15 +56,29 @@ public class TrackService extends Service {
                         .build();
 
         startForeground(ONGOING_NOTIFICATION_ID, notification);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    Log.d(TAG, location.toString());
+                }
+            }
+        };
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(TAG, "onStartCommand");
         if (isStarted) {
-            stopSelf();
+            stopService();
         }
         isStarted = true;
+        startLocationUpdates();
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -57,5 +86,31 @@ public class TrackService extends Service {
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy");
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
     }
+
+    private LocationRequest createLocationRequest() {
+        return new LocationRequest()
+                .setInterval(LOCATION_INTERVAL)
+                .setFastestInterval(LOCATION_INTERVAL)
+                .setSmallestDisplacement(LOCATION_DISTANCE)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    private void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            stopService();
+            return;
+        }
+
+        mFusedLocationClient.requestLocationUpdates(createLocationRequest(),
+                mLocationCallback,
+                null /* Looper */);
+    }
+
+    private void stopService() {
+        stopSelf();
+    }
+
 }
